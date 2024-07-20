e947ba13014bcc60b5ee18304c28296f54d13db2
Kadinsamson <l.kadinsamson@gmail.com>
2024-07-20 11:45:09 +0530

    portfolio

---
+++ src/Components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <center>
+        <h2>About Me</h2>
+    </center>
+    <ul>
+<li>Hello!! I am Kadin Samson L , a final-year student currently pursuing Artificial Intelligence & Data Science in Saveetha Engineering College, CHENNAI.</li>
+<li>I Did my schooling in BEST School CBSE , Kanniyakumari.</li>
+<li>I have scored 88% in Board Examination and currently with a CGPA of 7.4 as of semester-6.</li>
+<li>I am intrested in C,Python,C#,MySql,Database,HTML,CSS and React . </li>
+<li>I Did  internship in Axis global Limited on Industrial Robotics. </li>
+<li>Languages known are English, Tamil</li>
+</ul>
+  </section>
+);
+
+export default About;
---
+++ src/Components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact">
+    <h2>Contact</h2>
+    <ul>
+<li><a href="mailto:your-email@example.com">kadinsamson@gmail.com</a></li>
+<li><a href="mailto:your-email@example.com">www.linkedin.com/in/Kadin Samson L/</a></li>
+<li><a href="mailto:your-email@example.com">github.com/Kadinsamson</a></li>
+<li>Contact: 9080460564</li>
+</ul>
+  </section>
+);
+export default Contact;
